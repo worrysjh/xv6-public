970057a657fdf37229d238ba53d007f49a2579eb
worrysjh <97208639+worrysjh@users.noreply.github.com>
2022-04-22 21:53:00 +0900

    Update practicedate.c

--- practicedate.c
+++ practicedate.c
@@ -1,6 +1,6 @@
 #include "types.h"
 #include "user.h"
-#include "practicedate.h"
+#include "date.h"
 
 int main(int argc, char *argv[]){
  struct rtcdate r;
@@ -8,6 +8,6 @@ int main(int argc, char *argv[]){
     printf(2, "date failed\n");
     exit();
   }
-  printf(1, "%d-%d-%d, %d:%d:%d", r.year, r.month, r.day, r.hout, r.minute, r.second);
+  printf(1, "%d-%d-%d, %d:%d:%d", r.year, r.month, r.day, r.hour, r.minute, r.second);
   exit();
 }
