409787bbd9e5fc2efd93a5f1b026aa115ac8d71d
worrysjh <97208639+worrysjh@users.noreply.github.com>
2022-04-22 19:11:15 +0900

    Create date.c

---
+++ date.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "user.h"
+#incliude "date.h"
+
+int main(int argc, char *argv[]){
+ struct rtcdate r;
+  if(date(&r)) {
+    printf(2, "date failed\n");
+    exit();
+  }
+  printf(1, "%d-%d-%d, %d:%d:%d", r.year, r.month, r.day, r.hout, r.minute, r.second);
+  exit();
+}
